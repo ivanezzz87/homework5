887978a877cda5d3406b0b6aa1f1b0a47f604361
”Ivan <mseptk@yandex.ru>
2021-04-05 01:35:36 +0300

    создание классов и методов

--- src/belhard/java28/Main.java
+++ src/belhard/java28/Main.java
@@ -1,8 +1,13 @@
 package belhard.java28;
 
+import belhard.java28.entity.User;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	User user1 = new User("fm@gmail.com", "123", "Freddie", "Mercury");
+	user1.GetFullInfo();
+	user1.GetFullName();
+	System.out.println(user1.IsPasswordCorrect("321"));
     }
 }
--- src/belhard/java28/entity/User.java
+++ src/belhard/java28/entity/User.java
@@ -3,8 +3,18 @@ package belhard.java28.entity;
 public class User extends Person{
 private String email;
 private String password;
-    public User (String inFirstName, String inLastName) {
+    public User (String inEmail, String inPassword, String inFirstName, String inLastName) {
         super(inFirstName, inLastName);
-
+        this.email = inEmail;
+        this.password = inPassword;
+    }
+    public void GetFullInfo () {
+        System.out.println("Имя: " + this.firstName + " " + this.lastName);
+        System.out.println("EMail: " + this.email);
+    }
+    public boolean IsPasswordCorrect (String inPass) {
+        if (inPass.equals(this.password) ) {
+            return true;
+        } else {return false;}
     }
 }
